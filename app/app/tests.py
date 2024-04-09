5514ca7fb88c7cebd51e331dd90b2145a81c96be
ggarmatter <guilherme.jose@nexusinvestimentos.com.br>
2024-04-09 13:47:40 -0300

    feat: new test

--- app/app/tests.py
+++ app/app/tests.py
@@ -14,3 +14,8 @@ class CalcTests(SimpleTestCase):
         """test adding numbers"""
         res = calc.add(5, 6)
         self.assertEqual(res, 11)
+
+    def test_subtract_numbers(self):
+        """test subtracting numbers"""
+        res = calc.subtract(5, 6)
+        self.assertEqual(res, -1)
