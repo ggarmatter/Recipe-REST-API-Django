cb636cdd952c23cd37feb7c62aee47d5dbc48767
ggarmatter <guilherme.jose@nexusinvestimentos.com.br>
2024-05-03 11:51:33 -0300

    recipe api

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -1,7 +1,11 @@
 """Test user model"""
 
+from decimal import Decimal
+
 from django.test import TestCase
 from django.contrib.auth import get_user_model
+
+from core import models
 
 
 class ModelTests(TestCase):
@@ -40,3 +44,24 @@ class ModelTests(TestCase):
         )
         self.assertTrue(user.is_superuser)
         self.assertTrue(user.is_staff)
+
+    def test_create_recipe(self):
+        """Test creating a recipe"""
+
+        # cria usuário pra meter na receita
+        user = get_user_model().objects.create_user(
+            "test@example.com",
+            "testpass123",
+        )
+
+        title = "Sample recipe"
+
+        recipe = models.Recipe.objects.create(
+            user=user,
+            title=title,
+            time_minutes=5,
+            price=Decimal("5.50"),
+            description="Sample recipe description",
+        )
+
+        self.assertEqual(title, recipe.title)
---
+++ app/recipe/views.py
@@ -0,0 +1,36 @@
+"""Views for the receipe api"""
+
+from rest_framework import viewsets
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
+
+from core.models import Recipe
+from recipe import serializers
+
+
+class RecipeViewSet(viewsets.ModelViewSet):
+    """View for manage recipe API"""
+
+    # serializer converte os dados do model (database)
+    # para objetos python que podem ser usados para view
+    serializer_class = serializers.RecipeDetailSerializer
+    # diz quais objetos serão acessíveis por essa view
+    queryset = Recipe.objects.all()
+    # autenticacao
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        """Retrieve recipes forauthenticated user"""
+        return self.queryset.filter(user=self.request.user).order_by("-id")
+
+    def get_serializer_class(self):
+        """Return the serializer class for request"""
+        if self.action == "list":
+            return serializers.RecipeSerializer
+
+        return self.serializer_class
+
+    def perform_create(self, serializer):
+        """Create a new recipe"""
+        serializer.save(user=self.request.user)
